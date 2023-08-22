6f5f1447b72760cf36691043fb4a50ef8dcf6989
Gustavo Costa <gustavo1204.gc@gmail.com>
2023-08-21 22:15:23 -0300

    Complete Navbar, Adjust page offset

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen container-wrapper">
+    <main className="flex min-h-[calc(100vh - 4rem)] container-wrapper">
       Hello World
     </main>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
-import Link from 'next/link'
-import { LiaRobotSolid } from "react-icons/lia"
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { LiaRobotSolid } from "react-icons/lia";
+import { IoMenuOutline } from "react-icons/io5";
 
 interface NavItemProps {
   name: string;
@@ -7,24 +10,51 @@ interface NavItemProps {
 }
 
 function NavItem({ name, path }: NavItemProps) {
-  return <li className="text-description">
-    <Link href={path}>{name}</Link>
-  </li>
+  return (
+    <li className="text-description transition-all duration-200 ease-out hover:text-primary hover:scale-105">
+      <Link href={path}>{name}</Link>
+    </li>
+  );
+}
+
+function MobileNavItem({ name, path }: NavItemProps) {
+  return (
+    <li className="text-primary text-center w-full py-2 ">
+      <Link href={path}>{name}</Link>
+    </li>
+  );
 }
 
 export default function Navbar() {
-  return <nav className="flex h-16 w-full px-4 shadow-md">
-    <div className="flex items-center gap-2">
-      <Link href="/">
-        <LiaRobotSolid 
-          className="text-primary/70 text-[2.25rem]"
-        />
-      </Link>
-      <h2 className="text-xl text-description">UFC Autobots</h2>
-    </div>
-    <ul className="flex items-center gap-4 ml-auto">
-      <NavItem path="/" name="Alterar Senha" />
-      <NavItem path="/" name="Encerrar Sessão" />
-    </ul>
-  </nav>
+  const [open, setOpen] = useState<boolean>(false);
+
+  function toggleMenu() {
+    setOpen((prev) => (!prev));
+  }
+
+  return (
+    <nav className="relative flex h-16 w-full px-4 shadow-md">
+      <div className="flex items-center gap-2">
+        <Link href="/">
+          <LiaRobotSolid className="text-primary/70 text-[2.25rem]" />
+        </Link>
+        <h2 className="text-xl text-description">UFC Autobots</h2>
+      </div>
+      <ul className="hidden md:flex items-center gap-4 ml-auto">
+        <NavItem path="/" name="Alterar Senha" />
+        <NavItem path="/" name="Encerrar Sessão" />
+      </ul>
+      <button className="ml-auto md:hidden" onClick={() => toggleMenu()}>
+        <IoMenuOutline className="text-description text-2xl" />
+      </button>
+      {open && (
+        <div className="absolute right-0 top-16 py-4 w-full bg-white shadow-md">
+          <ul className="flex flex-col px-4">
+            <MobileNavItem path="/" name="Alterar Senha" />
+            <MobileNavItem path="/" name="Encerrar Sessão" />
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
 }
