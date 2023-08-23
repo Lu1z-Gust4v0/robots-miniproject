f4d9f2f1cf4d9b34e42f3bc2d1272428b85fd617
Gustavo Costa <gustavo1204.gc@gmail.com>
2023-08-22 23:56:39 -0300

    Add mock robot data

--- src/components/CarouselSection.tsx
+++ src/components/CarouselSection.tsx
@@ -3,8 +3,7 @@ import Carousel from "@/components/carousel/Carousel";
 import CarouselSlide from "@/components/carousel/CarouselSlide";
 import useWindowSize from "@/hooks/useWindowSize";
 import RobotCard from "@/components/RobotCard";
-
-const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+import { robots } from "@/data/robots"
 
 export default function CarouselSection() {
   const { width } = useWindowSize();
@@ -21,16 +20,16 @@ export default function CarouselSection() {
       <Carousel
         show={slidesCount}
       >
-        {NUMBERS.map((number, index) => (
+        {robots.map((robot, index) => (
           <CarouselSlide
             className="flex justify-center px-4 py-6 items-center md:w-1/2 lg:w-1/3 xl:w-1/4"
-            key={"number-" + number}
+            key={"slide-" + index}
             position={index}
           >
             <RobotCard
-              id={"robot-" + number}
-              name={"robot " + number}
-              description={"Bot responsabel por fazer web scrapping e analise de linguagem natural"}
+              id={robot.id}
+              name={robot.name}
+              description={robot.description}
             />
           </CarouselSlide>
         ))}
---
+++ src/data/robots.ts
@@ -0,0 +1,43 @@
+interface Robot{
+  id: string;
+  name: string;
+  description: string;
+} 
+
+export const robots: Robot[] = [
+  {
+    id: "robot01",
+    name: "Robot 01",
+    description: "Bot para utilizado para webscramping",
+  },
+  {
+    id: "robot02",
+    name: "Robot 02",
+    description: "Bot para utilizado para reconhecimento de linguagem natural",
+  },
+  {
+    id: "robot03",
+    name: "Robot 03",
+    description: "Bot para utilizado para tocar músicas no discord",
+  },
+  {
+    id: "robot04",
+    name: "Robot 04",
+    description: "Bot para utilizado para webscramping 2",
+  },
+  {
+    id: "robot05",
+    name: "Robot 05",
+    description: "Bot para utilizado para fazer buscar automaticas no google",
+  },
+  {
+    id: "robot06",
+    name: "Robot 06",
+    description: "Bot para utilizado para processar dados de imagem",
+  },
+  {
+    id: "robot07",
+    name: "Robot 07",
+    description: "Bot para utilizado para processar dados de imagem 2",
+  },
+]
