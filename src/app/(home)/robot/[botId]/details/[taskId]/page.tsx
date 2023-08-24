672b5cbf72f251de6cddad8ea59e5c8493be87b1
Gustavo Costa <gustavo1204.gc@gmail.com>
2023-08-24 19:12:59 -0300

    Add task card to details page

--- src/app/(home)/robot/[botId]/details/[taskId]/page.tsx
+++ src/app/(home)/robot/[botId]/details/[taskId]/page.tsx
@@ -2,6 +2,7 @@ import { Table, TableBody, TableHeader, detailsToRows } from "@/components/Table
 import type { Metadata } from "next";
 import { Error, isError } from "@/utils/error";
 import LinkButton from "@/components/LinkButton";
+import TaskCard from "@/components/TaskCard";
 
 export const metadata: Metadata = {
   title: "UFC Autobots • Details",
@@ -11,11 +12,31 @@ interface DetailsPageProps {
   params: { botId: string, taskId: string };
 }
 
-interface GetDataResponse {
+interface ExecutionDetailsResponse {
   content: string[];
 }
 
-async function getData(id: string): Promise<GetDataResponse | Error> {
+interface GetTaskResponse {
+  content: string[],
+  error?: string;
+}
+
+async function getTaskById(id: string): Promise<GetTaskResponse | Error> {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_URL}/api/task?id=${id}`,
+    {
+      cache: "no-store",
+    },
+  );
+
+  if (!response.ok) {
+    return { message: "failed to fetch data" };
+  }
+
+  return response.json();
+}
+
+async function getExecutionDetails(id: string): Promise<ExecutionDetailsResponse | Error> {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_URL}/api/details?id=${id}`,
     {
@@ -31,9 +52,12 @@ async function getData(id: string): Promise<GetDataResponse | Error> {
 }
 
 export default async function Details({ params }: DetailsPageProps) {
-  const data = await getData(params.taskId);
+  const details = await getExecutionDetails(params.taskId);
+  const task = await getTaskById(params.taskId);
 
   const headers = ["Data / Hora", "Mensagem"]
+
+  console.log(task)
 
   return (
     <main className="flex flex-col min-h-[calc(100vh - 4rem)] container-wrapper py-4">
@@ -41,12 +65,19 @@ export default async function Details({ params }: DetailsPageProps) {
         <h2 className="text-description text-3xl">
           Bot <span className="text-primary">{params.botId}</span>
         </h2>
-        <h3 className="text-description">Informações sobre a execução</h3>
       </section>
+      {!isError(task) && (
+        <section className="w-full my-2">
+          <TaskCard content={task.content} /> 
+        </section>
+      )}
       <section className="w-full my-4 py-4 overflow-x-auto">
+        <h2 className="text-description text-xl mb-2">
+          Detalhes da execução 
+        </h2>
         <Table>
           <TableHeader headers={headers} />
-          {!isError(data) && <TableBody rows={detailsToRows(data.content)} />}
+          {!isError(details) && <TableBody rows={detailsToRows(details.content)} />}
         </Table>
       </section>
       <section className="flex w-full p-4 justify-end">
