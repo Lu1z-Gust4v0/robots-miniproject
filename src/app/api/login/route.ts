46b80f58023bfe071f456e293673116b14095118
Gustavo Costa <gustavo1204.gc@gmail.com>
2023-08-26 15:10:28 -0300

    Add login endpoint

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import path from "path";
+import fs from "fs";
+import readline from "readline";
+import { removeWhitespace } from "@/utils/string";
+
+interface ILoginForm {
+  user: string;
+  password: string;
+}
+
+export async function POST(request: Request) {
+  const { user, password } = await request.json() as ILoginForm;
+
+  const dataDirectory = path.join(`${process.cwd()}/public/data`);
+
+  const fileStream = fs.createReadStream(
+    `${dataDirectory}/credentials.txt`,
+    "utf8",
+  );
+
+  const reader = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  for await (const line of reader) {
+    const [lineUser, _, linePassword] = line.split(",");
+
+    if (
+      lineUser === removeWhitespace(user) &&
+      linePassword === removeWhitespace(password)
+    ) {
+      return NextResponse.json({
+        message: "Credentials confirmed successfully",
+      }, { status: 200 });
+    }
+  }
+
+  fileStream.close();
+
+  return NextResponse.json({ message: "Invalid credentials" }, { status: 400 });
+}
